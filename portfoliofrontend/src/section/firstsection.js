35e14b171dc28fdd1e57a599cb6c390d1ae9cd29
appsec.andrewlee <appsec.andrewlee@gmail.com>
2024-04-05 03:26:07 +1100

    changes

--- portfoliofrontend/src/section/firstsection.js
+++ portfoliofrontend/src/section/firstsection.js
@@ -48,7 +48,7 @@ function FirstSection() {
                 duration={500}
               >
                 <img
-                  src="/down_arrow.png"
+                  src="/down.png"
                   alt="Down arrow"
                   style={{
                     animation: "slideDown 1.5s infinite",
