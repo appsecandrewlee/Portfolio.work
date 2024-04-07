efbd42ff7166ecae89bbb3649bf2a82733bea85b
appsec.andrewlee <appsec.andrewlee@gmail.com>
2024-04-07 18:20:25 +1000

    changed naming conventions for the images

--- portfoliofrontend/src/section/firstsection.js
+++ portfoliofrontend/src/section/firstsection.js
@@ -48,7 +48,7 @@ function FirstSection() {
                 duration={500}
               >
                 <img
-                  src={process.env.PUBLIC_URL + "/down.png"}
+                  src={process.env.PUBLIC_URL + "/DOWN.png"}
                   alt="Down arrow"
                   style={{
                     animation: "slideDown 1.5s infinite",
