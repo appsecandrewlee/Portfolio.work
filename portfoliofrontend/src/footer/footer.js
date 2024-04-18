59a9ae2f91826ca524d3c135ca04bac6ac2936d5
appsec.andrewlee <appsec.andrewlee@gmail.com>
2024-04-18 14:07:25 +1000

    implementation of rocket animation is done, moving onto fixing gray area compatibility

--- portfoliofrontend/src/footer/footer.js
+++ portfoliofrontend/src/footer/footer.js
@@ -1,26 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 import "./footer.css";
 
 function Footer() {
-  const [hovered, setHovered] = useState(false);
+  const [animationStarted, setAnimationStarted] = useState(false);
+
+  const handleMouseEnter = () => {
+    if (!animationStarted) {
+      setAnimationStarted(true);
+      setTimeout(() => {
+        setAnimationStarted(false);
+      }, 3500); 
+    }
+  };
 
   return (
     <div className="text-center" style={{ marginTop: "2vh" }}>
-      <Link to="aboutme" spy={true} smooth={true} offset={-70} duration={500}>
-        <div>
-          <img
-            src={process.env.PUBLIC_URL + "/rocket.png"}
-            alt="Rocket Logo"
-            className="rocket-logo"
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
-          />
-        </div>
-      </Link>
+      <div style={{ position: "relative", display: "inline-block" }}>
+        <Link to="aboutme" spy={true} smooth={true} offset={-70} duration={500}>
+          <div>
+            <img
+              src={process.env.PUBLIC_URL + "/rocket.png"}
+              alt="Rocket Logo"
+              className={`rocket-logo ${animationStarted ? "rocket-fly" : ""}`}
+              onMouseEnter={handleMouseEnter}
+            />
+          </div>
+        </Link>
+      </div>
       <Link to="aboutme" spy={true} smooth={true} offset={-70} duration={500}>
         <p className="footer-text">
-          {hovered ? "Click Here" : "© 2024 Andrew Lee"}
+          {animationStarted ? "Click Here" : "© 2024 Andrew Lee"}
         </p>
       </Link>
     </div>
@@ -28,5 +38,3 @@ function Footer() {
 }
 
 export default Footer;
-
-
