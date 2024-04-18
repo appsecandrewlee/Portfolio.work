82d1f9ef4d5b4c3eafbc2049a43ab99c927fafaf
appsec.andrewlee <appsec.andrewlee@gmail.com>
2024-04-18 13:58:28 +1000

    implementation of rocket.png for copyright, nearly done reaching there

--- portfoliofrontend/src/footer/footer.js
+++ portfoliofrontend/src/footer/footer.js
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import { Link } from "react-scroll";
 import "./footer.css";
+
 function Footer() {
   const [hovered, setHovered] = useState(false);
 
   return (
-    <div className="text-center">
+    <div className="text-center" style={{ marginTop: "2vh" }}>
       <Link to="aboutme" spy={true} smooth={true} offset={-70} duration={500}>
-        <p
-          className="footer-text" // Added a class name for easier styling
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-        >
+        <div>
+          <img
+            src={process.env.PUBLIC_URL + "/rocket.png"}
+            alt="Rocket Logo"
+            className="rocket-logo"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          />
+        </div>
+      </Link>
+      <Link to="aboutme" spy={true} smooth={true} offset={-70} duration={500}>
+        <p className="footer-text">
           {hovered ? "Click Here" : "© 2024 Andrew Lee"}
         </p>
       </Link>
@@ -20,3 +28,5 @@ function Footer() {
 }
 
 export default Footer;
+
+
